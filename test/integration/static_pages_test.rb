8cd54e8f5297e99a96295db9dc8a0729040855b6
ldrohan <drohanluke@gmail.com>
2014-08-23 13:40:48 -0700

    added about page

---
+++ test/integration/static_pages_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class StaticPagesTest < ActionDispatch::IntegrationTest
+ describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+  end
+end
